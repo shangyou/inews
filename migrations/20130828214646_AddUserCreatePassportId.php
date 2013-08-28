ea59f38a27b06118dd62d19b08ee529192d6f3b1
hfcorriez <hfcorriez@gmail.com>
2013-08-28 21:53:48 +0800

    Fix migration create with passport id

--- migrations/20130828214646_AddUserCreatePassportId.php
+++ migrations/20130828214646_AddUserCreatePassportId.php
@@ -9,7 +9,7 @@ class AddUserCreatePassportId extends Migration
      */
     public function up()
     {
-        $this->container['pdo']->exec('ALTER TABLE `user` MODIFY COLUMN `create_passport_id` INT(11) DEFAULT NULL AFTER `status`;');
+        $this->container['pdo']->exec('ALTER TABLE `user` ADD `create_passport_id` INT  NULL  DEFAULT NULL  AFTER `status`;');
     }
 
     /**
